dcdef0b9809702baf17d2997be5e16d05b18f031
Arsenij04022006 <metkurbanovarsenij@gmail.com>
2021-12-20 21:25:58 +0300

    123345

--- Duck.py
+++ Duck.py
@@ -1,22 +1,55 @@
+import random
+from turtle import width
+
 import pygame
 
-n = int(input())
-width1 = 40
-height1 = 300
-a = 136.5
-b = 0
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 300, 300
-    screen = pygame.display.set_mode(size)
-    pygame.draw.ellipse(screen, (255, 255, 255), (0, 0, 300, 300), 1)
-    pygame.display.flip()
-    for i in range(1, n):
-        pygame.draw.ellipse(screen, (255, 255, 255), (a, b, width1, height1), 1)
-        pygame.draw.ellipse(screen, (255, 255, 255), (b, a, height1, width1), 1)
-        pygame.display.flip()
-        width1 += 28
-        a -= 15
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y, all_sprites=None):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+
+
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+
+
+class Border(pygame.sprite.Sprite):
+
+    def __init__(self, x1, y1, x2, y2, all_sprites=None, vertical_borders=None, horizontal_borders=None):
+        super().__init__(all_sprites)
+        if x1 == x2:
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
+Border(5, 5, width - 5, 5)
+Border(5, height - 5, width - 5, height - 5)
+Border(5, 5, 5, height - 5)
+Border(width - 5, 5, width - 5, height - 5)
+
+for i in range(10):
+    Ball(20, 100, 100)
+
+
+def update(self):
+    self.rect = self.rect.move(self.vx, self.vy)
+    if pygame.sprite.spritecollideany(self, horizontal_borders):
+        self.vy = -self.vy
+    if pygame.sprite.spritecollideany(self, vertical_borders):
+        self.vx = -self.vx
