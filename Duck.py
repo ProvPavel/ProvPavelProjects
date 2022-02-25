4c4c9048a629fa36f177694557216b9e422e9868
Arsenij04022006 <metkurbanovarsenij@gmail.com>
2022-02-25 20:56:25 +0300

    hgcfjkcf

--- Duck.py
+++ Duck.py
@@ -1,250 +1,217 @@
-from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QComboBox, \
-    QPushButton, QLCDNumber, QCheckBox, QLabel
-from PyQt5.QtGui import QPainter
-import sys
+import pygame
+import random
+
+colors = [
+    (0, 0, 0),
+    (120, 37, 179),
+    (100, 179, 179),
+    (80, 34, 22),
+    (80, 134, 22),
+    (180, 34, 22),
+    (180, 34, 122),
+]
 
 
-class Window(QWidget):
-    def __init__(self, TEXT_COLOR=None, BACKGROUND_COLOR=None, BORDER_SIZE=None, BORDER_COLOR=None):
-        super(Window, self).__init__()
+class Figure:
+    x = 0
+    y = 0
 
-        self.setWindowTitle('Калькулятор')
-        self.setGeometry(400, 100, 100, 100)
-        self.setFixedSize(425, 625)
+    figures = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]],
+        [[4, 5, 9, 10], [2, 6, 5, 9]],
+        [[6, 7, 9, 10], [1, 5, 6, 10]],
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
+        [[1, 2, 5, 6]],
+    ]
 
-        self.result = QLineEdit(self)
-        self.result.move(25, 25)
-        self.result.resize(375, 75)
-        self.result.setStyleSheet('background-color: rgb(255,255,255); '
-                                  f'font-size: 10px; '
-                                  f'border: 5px solid rgb(0,0,0)')
-        self.result.setDisabled(True)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.randint(0, len(self.figures) - 1)
+        self.color = random.randint(1, len(colors) - 1)
+        self.rotation = 0
 
-        self.button_7 = QPushButton(self)
-        self.button_7.move(25, 125)
-        self.button_7.resize(75, 75)
-        self.button_7.setText('7')
-        self.button_7.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_7.clicked.connect(self.count)
+    def image(self):
+        return self.figures[self.type][self.rotation]
 
-        self.button_8 = QPushButton(self)
-        self.button_8.move(125, 125)
-        self.button_8.resize(75, 75)
-        self.button_8.setText('8')
-        self.button_8.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_8.clicked.connect(self.count)
-
-        self.button_9 = QPushButton(self)
-        self.button_9.move(225, 125)
-        self.button_9.resize(75, 75)
-        self.button_9.setText('9')
-        self.button_9.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_9.clicked.connect(self.count)
-
-        self.res_1 = QPushButton(self)
-        self.res_1.move(325, 125)
-        self.res_1.resize(75, 75)
-        self.res_1.setText('+')
-        self.res_1.setStyleSheet('background-color: rgb(0,255,0); '
-                                 f'font-size: 15px; '
-                                 f'border: 5px solid rgb(0,0,0)')
-        self.res_1.clicked.connect(self.count)
-
-        self.button_4 = QPushButton(self)
-        self.button_4.move(25, 225)
-        self.button_4.resize(75, 75)
-        self.button_4.setText('4')
-        self.button_4.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_4.clicked.connect(self.count)
-
-        self.button_5 = QPushButton(self)
-        self.button_5.move(125, 225)
-        self.button_5.resize(75, 75)
-        self.button_5.setText('5')
-        self.button_5.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_5.clicked.connect(self.count)
-
-        self.button_6 = QPushButton(self)
-        self.button_6.move(225, 225)
-        self.button_6.resize(75, 75)
-        self.button_6.setText('6')
-        self.button_6.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_6.clicked.connect(self.count)
-
-        self.res_2 = QPushButton(self)
-        self.res_2.move(325, 225)
-        self.res_2.resize(75, 75)
-        self.res_2.setText('-')
-        self.res_2.setStyleSheet('background-color: rgb(0,255,0); '
-                                 f'font-size: 15px; '
-                                 f'border: 5px solid rgb(0,0,0)')
-        self.res_2.clicked.connect(self.count)
-
-        self.button_1 = QPushButton(self)
-        self.button_1.move(25, 325)
-        self.button_1.resize(75, 75)
-        self.button_1.setText('1')
-        self.button_1.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_1.clicked.connect(self.count)
-
-        self.button_2 = QPushButton(self)
-        self.button_2.move(125, 325)
-        self.button_2.resize(75, 75)
-        self.button_2.setText('2')
-        self.button_2.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_2.clicked.connect(self.count)
-
-        self.button_3 = QPushButton(self)
-        self.button_3.move(225, 325)
-        self.button_3.resize(75, 75)
-        self.button_3.setText('3')
-        self.button_3.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_3.clicked.connect(self.count)
-
-        self.res_3 = QPushButton(self)
-        self.res_3.move(325, 325)
-        self.res_3.resize(75, 75)
-        self.res_3.setText('*')
-        self.res_3.setStyleSheet('background-color: rgb(0, 255, 0);'
-                                 f'font-size: 15px; '
-                                 f'border: 5px solid rgb(0,0,0)')
-        self.res_3.clicked.connect(self.count)
-
-        self.button_C = QPushButton(self)
-        self.button_C.move(25, 425)
-        self.button_C.resize(75, 75)
-        self.button_C.setText('C')
-        self.button_C.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_C.clicked.connect(self.count)
-
-        self.button_0 = QPushButton(self)
-        self.button_0.move(125, 425)
-        self.button_0.resize(75, 75)
-        self.button_0.setText('0')
-        self.button_0.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 15px; '
-                                    f'border: 5px solid rgb(0,0,0)')
-        self.button_0.clicked.connect(self.count)
-
-        self.button_point = QPushButton(self)
-        self.button_point.move(225, 425)
-        self.button_point.resize(75, 75)
-        self.button_point.setText('.')
-        self.button_point.setStyleSheet('background-color: rgb(0,255,0); '
-                                        f'font-size: 15px; '
-                                        f'border: 5px solid rgb(0,0,0)')
-        self.button_point.clicked.connect(self.count)
-
-        self.res_4 = QPushButton(self)
-        self.res_4.move(325, 425)
-        self.res_4.resize(75, 75)
-        self.res_4.setText('/')
-        self.res_4.setStyleSheet('background-color: rgb(0,255,0); '
-                                 f'font-size: 15px; '
-                                 f'border: 5px solid rgb(0,0,0)')
-        self.res_4.clicked.connect(self.count)
-
-        self.button_res = QPushButton(self)
-        self.button_res.move(25, 525)
-        self.button_res.resize(375, 75)
-        self.button_res.setText('=')
-        self.button_res.setStyleSheet('background-color: rgb(0,255,0); '
-                                      f'font-size: 15px; '
-                                      f'border: 5px solid rgb(0,0,0)')
-        self.button_res.clicked.connect(self.count)
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.figures[self.type])
 
 
-def paintEvent(self, event):
-    qp = QPainter(self)
-    qp.begin(self)
-    qp.setBrush(QColor(0, 200, 100))
-    qp.drawRect(0, 0, 425, 625)
-    qp.end()
+class Tetris:
+    level = 2
+    score = 0
+    state = "Start"
+    field = []
+    height = 0
+    width = 0
+    x = 100
+    y = 60
+    zoom = 20
+    figure = None
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.field = []
+        self.score = 0
+        self.state = "Start"
+        for i in range(height):
+            a = []
+            for j in range(width):
+                a.append(0)
+            self.field.append(a)
+
+    def new_figure(self):
+        self.figure = Figure(3, 0)
+
+    def intersects(self):
+        intersection = False
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.figure.image():
+                    if i + self.figure.y > self.height - 1 or \
+                            j + self.figure.x > self.width - 1 or \
+                            j + self.figure.x < 0 or \
+                            self.field[i + self.figure.y][j + self.figure.x] > 0:
+                        intersection = True
+        return intersection
+
+    def break_lines(self):
+        lines = 0
+        for i in range(1, self.height):
+            zeros = 0
+            for j in range(self.width):
+                if self.field[i][j] == 0:
+                    zeros += 1
+            if zeros == 0:
+                lines += 1
+                for i1 in range(i, 1, -1):
+                    for j in range(self.width):
+                        self.field[i1][j] = self.field[i1 - 1][j]
+        self.score += lines ** 2
+
+    def go_space(self):
+        while not self.intersects():
+            self.figure.y += 1
+        self.figure.y -= 1
+        self.freeze()
+
+    def go_down(self):
+        self.figure.y += 1
+        if self.intersects():
+            self.figure.y -= 1
+            self.freeze()
+
+    def freeze(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.figure.image():
+                    self.field[i + self.figure.y][j + self.figure.x] = self.figure.color
+        self.break_lines()
+        self.new_figure()
+        if self.intersects():
+            self.state = "Game over"
+
+    def go_side(self, dx):
+        old_x = self.figure.x
+        self.figure.x += dx
+        if self.intersects():
+            self.figure.x = old_x
+
+    def rotate(self):
+        old_rotation = self.figure.rotation
+        self.figure.rotate()
+        if self.intersects():
+            self.figure.rotation = old_rotation
 
 
-def count(self):
-    try:
-        a = self.sender()
+# Инициализировать игровой движок
+pygame.init()
 
-        if a == self.button_7:
-            self.result.setText(self.result.text() + '7')
+# Определите некоторые цвета
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
 
-        elif a == self.button_8:
-            self.result.setText(self.result.text() + '8')
+size = (400, 500)
+screen = pygame.display.set_mode(size)
 
-        elif a == self.button_9:
-            self.result.setText(self.result.text() + '9')
+pygame.display.set_caption("Tetris")
 
-        elif a == self.res_1:
-            self.result.setText(self.result.text() + '+')
+# Цикл, пока пользователь не нажмет кнопку закрытия
+done = False
+clock = pygame.time.Clock()
+fps = 25
+game = Tetris(20, 10)
+counter = 0
 
-        elif a == self.button_4:
-            self.result.setText(self.result.text() + '4')
+pressing_down = False
 
-        elif a == self.button_5:
-            self.result.setText(self.result.text() + '5')
+while not done:
+    if game.figure is None:
+        game.new_figure()
+    counter += 1
+    if counter > 100000:
+        counter = 0
 
-        elif a == self.button_6:
-            self.result.setText(self.result.text() + '6')
+    if counter % (fps // game.level // 2) == 0 or pressing_down:
+        if game.state == "Start":
+            game.go_down()
 
-        elif a == self.res_2:
-            self.result.setText(self.result.text() + '-')
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                game.rotate()
+            if event.key == pygame.K_DOWN:
+                pressing_down = True
+            if event.key == pygame.K_LEFT:
+                game.go_side(-1)
+            if event.key == pygame.K_RIGHT:
+                game.go_side(1)
+            if event.key == pygame.K_SPACE:
+                game.go_space()
+            if event.key == pygame.K_ESCAPE:
+                game.__init__(20, 10)
 
-        elif a == self.button_1:
-            self.result.setText(self.result.text() + '1')
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_DOWN:
+            pressing_down = False
 
-        elif a == self.button_2:
-            self.result.setText(self.result.text() + '2')
+    screen.fill(WHITE)
 
-        elif a == self.button_3:
-            self.result.setText(self.result.text() + '3')
+    for i in range(game.height):
+        for j in range(game.width):
+            pygame.draw.rect(screen, GRAY, [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
+            if game.field[i][j] > 0:
+                pygame.draw.rect(screen, colors[game.field[i][j]],
+                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
 
-        elif a == self.res_3:
-            self.result.setText(self.result.text() + '*')
+    if game.figure is not None:
+        for i in range(4):
+            for j in range(4):
+                p = i * 4 + j
+                if p in game.figure.image():
+                    pygame.draw.rect(screen, colors[game.figure.color],
+                                     [game.x + game.zoom * (j + game.figure.x) + 1,
+                                      game.y + game.zoom * (i + game.figure.y) + 1,
+                                      game.zoom - 2, game.zoom - 2])
 
-        elif a == self.button_C:
-            self.result.setText('')
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+    font1 = pygame.font.SysFont('Calibri', 65, True, False)
+    text = font.render("Счёт: " + str(game.score), True, BLACK)
+    text_game_over = font1.render("Game Over", True, (255, 125, 0))
 
-        elif a == self.button_0:
-            self.result.setText(self.result.text() + '0')
+    screen.blit(text, [0, 0])
+    if game.state == "Game over":
+        screen.blit(text_game_over, [20, 200])
+        screen.blit(text, [25, 265])
 
-        elif a == self.button_point:
-            self.result.setText(self.result.text() + '.')
+    pygame.display.flip()
+    clock.tick(fps)
 
-        elif a == self.res_4:
-            self.result.setText(self.result.text() + '/')
-
-        elif a == self.button_res:
-            self.result.setText(str(eval(self.result.text())))
-
-    except Exception:
-        self.result.setText('Error')
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    wnd = Window()
-    wnd.show()
-    app.exec()
-    sys.exit(app.exec())
+pygame.quit()
--- QT 1.Калькулятор.py
+++ QT 1.Калькулятор.py
@@ -17,184 +17,120 @@ class Window(QWidget):
         self.result = QLineEdit(self)
         self.result.move(25, 25)
         self.result.resize(375, 75)
-        self.result.setStyleSheet('background-color: rgb(255,255,255); '
-                                  f'font-size: 30pt; '
-                                  f'color: rgb(100,155,100); '
-                                  f'border: 5px solid rgb(0,0,0)')
         self.result.setDisabled(True)
 
         self.button_7 = QPushButton(self)
         self.button_7.move(25, 125)
         self.button_7.resize(75, 75)
         self.button_7.setText('7')
-        self.button_7.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_7.clicked.connect(self.count)
 
         self.button_8 = QPushButton(self)
         self.button_8.move(125, 125)
         self.button_8.resize(75, 75)
         self.button_8.setText('8')
-        self.button_8.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_8.clicked.connect(self.count)
 
         self.button_9 = QPushButton(self)
         self.button_9.move(225, 125)
         self.button_9.resize(75, 75)
         self.button_9.setText('9')
-        self.button_9.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_9.clicked.connect(self.count)
 
         self.res_1 = QPushButton(self)
         self.res_1.move(325, 125)
         self.res_1.resize(75, 75)
         self.res_1.setText('+')
-        self.res_1.setStyleSheet('background-color: rgb(0,255,0); '
-                                 f'font-size: 14px; '
-                                 f'border: 5px solid rgb(0,0,0)')
         self.res_1.clicked.connect(self.count)
 
         self.button_4 = QPushButton(self)
         self.button_4.move(25, 225)
         self.button_4.resize(75, 75)
         self.button_4.setText('4')
-        self.button_4.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_4.clicked.connect(self.count)
 
         self.button_5 = QPushButton(self)
         self.button_5.move(125, 225)
         self.button_5.resize(75, 75)
         self.button_5.setText('5')
-        self.button_5.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_5.clicked.connect(self.count)
 
         self.button_6 = QPushButton(self)
         self.button_6.move(225, 225)
         self.button_6.resize(75, 75)
         self.button_6.setText('6')
-        self.button_6.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_6.clicked.connect(self.count)
 
         self.res_2 = QPushButton(self)
         self.res_2.move(325, 225)
         self.res_2.resize(75, 75)
         self.res_2.setText('-')
-        self.res_2.setStyleSheet('background-color: rgb(0,255,0); '
-                                 f'font-size: 14px; '
-                                 f'border: 5px solid rgb(0,0,0)')
         self.res_2.clicked.connect(self.count)
 
         self.button_1 = QPushButton(self)
         self.button_1.move(25, 325)
         self.button_1.resize(75, 75)
         self.button_1.setText('1')
-        self.button_1.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_1.clicked.connect(self.count)
 
         self.button_2 = QPushButton(self)
         self.button_2.move(125, 325)
         self.button_2.resize(75, 75)
         self.button_2.setText('2')
-        self.button_2.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_2.clicked.connect(self.count)
 
         self.button_3 = QPushButton(self)
         self.button_3.move(225, 325)
         self.button_3.resize(75, 75)
         self.button_3.setText('3')
-        self.button_3.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_3.clicked.connect(self.count)
 
         self.res_3 = QPushButton(self)
         self.res_3.move(325, 325)
         self.res_3.resize(75, 75)
         self.res_3.setText('*')
-        self.res_3.setStyleSheet('background-color: rgb(0,255,0); '
-                                 f'font-size: 14px; '
-                                 f'border: 5px solid rgb(0,0,0)')
         self.res_3.clicked.connect(self.count)
 
         self.button_C = QPushButton(self)
         self.button_C.move(25, 425)
         self.button_C.resize(75, 75)
         self.button_C.setText('C')
-        self.button_C.setStyleSheet('background-color: rgb(0,255,0);'
-                                    f'font-size: 14px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_C.clicked.connect(self.count)
 
         self.button_0 = QPushButton(self)
         self.button_0.move(125, 425)
         self.button_0.resize(75, 75)
         self.button_0.setText('0')
-        self.button_0.setStyleSheet('background-color: rgb(0,255,0); '
-                                    f'font-size: 10px; '
-                                    f'border: 5px solid rgb(0,0,0)')
         self.button_0.clicked.connect(self.count)
 
         self.button_point = QPushButton(self)
         self.button_point.move(225, 425)
         self.button_point.resize(75, 75)
         self.button_point.setText('.')
-        self.button_point.setStyleSheet('background-color: rgb(0,255,0); '
-                                        f'font-size: 14px; '
-                                        f'border: 5px solid rgb(0,0,0)')
         self.button_point.clicked.connect(self.count)
 
         self.res_4 = QPushButton(self)
         self.res_4.move(325, 425)
         self.res_4.resize(75, 75)
         self.res_4.setText('/')
-        self.res_4.setStyleSheet('background-color: rgb(0,255,0); '
-                                 f'font-size: 14px; '
-                                 f'border: 5px solid rgb(0,0,0)')
         self.res_4.clicked.connect(self.count)
 
         self.button_AC = QPushButton(self)
         self.button_AC.move(25, 525)
         self.button_AC.resize(75, 75)
         self.button_AC.setText('AC')
-        self.button_AC.setStyleSheet('background-color: rgb(0,255,0); '
-                                     f'font-size: 14px; '
-                                     f'border: 5px solid rgb(0,0,0)')
         self.button_AC.clicked.connect(self.count)
 
         self.button_res = QPushButton(self)
         self.button_res.move(125, 525)
         self.button_res.resize(275, 75)
         self.button_res.setText('=')
-        self.button_res.setStyleSheet('background-color: rgb(0,255,0); '
-                                      f'font-size: 14px; '
-                                      f'border: 5px solid rgb(0,0,0)')
         self.button_res.clicked.connect(self.count)
 
-    def hiddenEgg(self):
-        p = self.result.text()
-        if p == '2022':
-            self.result.setText('Happy New Year :)')
-
-    # jcchchgc
     def paintEvent(self, event):
         qp = QPainter(self)
         qp.begin(self)
-        qp.setBrush(QColor(0, 200, 100))
+        qp.setBrush(QColor(255, 255, 255))
         qp.drawRect(0, 0, 425, 625)
         qp.end()
 
@@ -254,7 +190,6 @@ class Window(QWidget):
 
             elif a == self.button_res:
                 self.result.setText(str(eval(self.result.text())))
-                self.hiddenEgg()
 
         except Exception:
             self.result.setText('Error')
