f4cae69a89198977d116cbed99363ab94d2daf0b
Pavel <paulus40rus@gmail.com>
2022-02-28 21:14:28 +0300

    start

--- Argparse Проверка ошибок.py
+++ Argparse Проверка ошибок.py
@@ -1,10 +1,15 @@
 import argparse
 
 
-def print_error():
+def print_error(message):
+    if len(message) == 1:
+        message = message[0]
+    print(f'ERROR: {message}!!')
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('anything')
-    parser.parse_args()
-
-
-
+    parser.add_argument('message', nargs='*')
+    mess = parser.parse_args().message
+    print('Welcome to my program')
+    print_error(mess)
