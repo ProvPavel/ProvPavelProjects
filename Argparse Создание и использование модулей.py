7e53c4b961c7bf92310f2ee49a795126352d94bc
Arsenij04022006 <metkurbanovarsenij@gmail.com>
2022-03-21 19:33:02 +0300

    Duck

---
+++ Argparse Создание и использование модулей.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def count_lines(file):
+    try:
+        with open(file) as fl:
+            return len(fl.readlines())
+    except Exception:
+        return 0
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file")
+    args = parser.parse_args()
+
+    print(count_lines(args.file))
+
+
+if __name__ == '__main__':
+    main()
