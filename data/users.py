43e50a98cc8596d7607f730a60c926c830f24069
Pavel <paulus40rus@gmail.com>
2022-03-21 21:04:05 +0300

    flasktest

---
+++ data/users.py
@@ -0,0 +1,21 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    def __repr__(self):
+        for user in db_sess.query(User).all():
+            print(user)
+
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
